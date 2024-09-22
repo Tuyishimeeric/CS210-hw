03dd4246dcdfaf2513aa52d41c07372ad3e81cc2
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-09-22 18:02:28 +0200

    grading program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Data;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Enter your course score: ");
+        int score = int.Parse(Console.ReadLine());
+        string grade;
+        if (score >=90);
+        {
+            grade = "A";
+        }
+        else if(score >= 80);
+        {
+            grade = "B";
+        }
+        else if(grade>=70);
+        {
+            grade = "C";
+        }
+        else if(score >=60);
+        {
+            grade = "D";
+        
+        }
+        else
+        {
+        grade = "F";
+        }
+        Console.WriteLine($"your grade is: (grade)")
     }
+
 }
