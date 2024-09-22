8a11dff586235fb3e990dfc05f1450c811ac5079
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-09-22 21:12:48 +0200

    list

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,7 +1,26 @@
 using System;
 
-Console.WriteLine("What is your magic number?");
-int magicnum = int.Parse(Console.ReadLine());
+Random random = new Random();
+int randomnumber = random.Next(1,100);
 
-Console.WriteLine("What is your guess number");
-int guessnnum = int.Parse(Console.ReadLine());
+Console.WriteLine("What is your guess number between 1 annd 100");
+int guessnnum = 0;
+int attempts = 0;
+
+while (guessnnum != randomnumber )
+{
+   Console.WriteLine("What is your guess number: "); 
+   guessnnum = int.Parse(Console.ReadLine());
+   attempts++;
+
+if (guessnnum > randomnumber)
+{
+    Console.WriteLine(" your guess is Higher");
+}
+else if(guessnnum < randomnumber)
+{
+  Console.WriteLine("you guess is Lower");
+}
+}
+Console.WriteLine("Congratulation!!! you guessed right! you should buy yourself a cake!");
+Console.WriteLine($"it took you : {attempts}  attempts to guess the number");
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int usernum;
+        Console.WriteLine("enter a list of numbers, type 0 when finished");
+        do
+        {
+            usernum = int.Parse(Console.ReadLine());
+            if (usernum != 0)
+            {
+                numbers.Add(usernum);
+            }
+        }
+        while (usernum != 0);
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            int largest = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+            double average = (double)sum / numbers.Count;
+            Console.WriteLine($"sum of the numbers : {sum}");
+
+            Console.WriteLine($"average of the numbers: {average}");
+
+            Console.WriteLine($"the largest number in the list is: {largest}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered");
+        }
     }
 }
