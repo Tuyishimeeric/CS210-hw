374c8c0d44b3e7be8757443313b4b6db1c45f631
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-09-24 23:00:44 +0200

    Abstraction Resume activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,6 +7,27 @@ class Program
     {
         Console.WriteLine("Hello world");
 
-    }
+    
+    Job Job1 = new Job();
+        Job1._JobTitle =" Software Engineer";
+        Job1._CompanyName = "Microsoft ";
+        Job1._StartYear = 2019;
+        Job1._EndYear = 2022;
 
+    Job Job2 = new Job();
+        Job2._JobTitle = "Manager";
+        Job2._CompanyName = "Apple";
+        Job2._StartYear = 2022;
+        Job2._EndYear = 2023;
+
+        Job1. Display();
+        Job2. Display();
+        Resume myResume = new Resume();
+        myResume._Name = "TUYISHIME Eric";
+
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        myResume.Display();
+    }
 }
---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,15 @@
+using System;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Job
+    {
+      public string _JobTitle;
+      public string _CompanyName;
+      public int _StartYear;
+      public int _EndYear;
+      public void Display()
+      {
+        Console.WriteLine($"{_JobTitle} {_CompanyName} {_StartYear} {_EndYear}");
+      }
+    }
